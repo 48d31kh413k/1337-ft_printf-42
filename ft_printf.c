c9b895597b7c31fe5c4d92493ee7a25369a92a8f
48d31kh413k <65833007+48d31kh413k@users.noreply.github.com>
2021-10-19 17:35:17 +0100

    Add Mandatory Part

---
+++ ft_printf.c
@@ -0,0 +1,30 @@
+#include "ft_printf.c"
+
+
+
+
+
+
+int	ft_printf(const char *format, ...)
+{
+	int	i;
+	va_list	vl;
+	int	char_printed;
+
+	i = 0;
+	char_printed = 0;
+	va_start(vl, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			char_printed += ft_conversion(vl, str[i + 1]);
+			i++;
+		}
+		else
+			char_printed += ft_putchar(format[i]);
+		i++;
+	}
+	va_end(vl);
+	return (char_printed);
+}
